1400029306a330b3a56ce83548df88d8adbb497e
santomon <37147575+santomon@users.noreply.github.com>
2021-12-03 19:23:55 +0100

    Update VIBV4.py

--- VIBV4.py
+++ VIBV4.py
@@ -8,7 +8,7 @@ tf.compat.v1.reset_default_graph()
 
 # Turn on xla optimization
 config = tf.compat.v1.ConfigProto()
-config.graph_options.optimizer_options.global_jit_level = tf.OptimizerOptions.ON_1
+config.graph_options.optimizer_options.global_jit_level = tf.compat.v1.OptimizerOptions.ON_1
 sess = tf.InteractiveSession(config=config)
 
 
